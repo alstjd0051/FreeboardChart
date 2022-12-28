87142b2e1491beeea5dca8382fababc51d23a0d3
alstjd0051 <wsc03002@naver.com>
2022-12-28 14:01:12 +0900

    update:: Chart

--- components/commons/chart/LineChart.tsx
+++ components/commons/chart/LineChart.tsx
@@ -10,8 +10,17 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
 
-ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 interface Props {
   data?: Object;
@@ -22,36 +31,41 @@ export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: "top" as const,
+      // position: "top" as const,
+      display: false,
     },
     title: {
-      display: true,
-      text: "Chart.js Line Chart",
+      display: false,
     },
   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+const labels = [
+  "8/19",
+  "9/19",
+  "19/19",
+  "11/19",
+  "12/19",
+  "1/20",
+  "2/20",
+  "3/20",
+  "4/20",
+  "5/20",
+];
 
 export const data = {
   labels,
   datasets: [
     {
-      label: "Dataset 1",
-      data: labels.map(() => ({ min: -1000, max: 1000 })),
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => ({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
   ],
 };
 
 const LineChart = (props: Props) => {
+  ChartJS.defaults.font.size = 10;
   return (
     <div className="relative ">
       <Line data={data} options={options} />
